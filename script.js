97383ab81bc1440e69088944bd278585d3817efa
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-02-11 19:07:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(a => {
+    if (a.marks > 50) {
+     console.log(a);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(a => {
+    if (a.marks > 50) {
+      console.log(a);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newArr = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newArr);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(a => a.marks >= 50);
+  console.log(arr);
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 7, name: "Dhanvanti", age: "20", marks: 85 },
+    { id: 9, name: "Priya", age: "20", marks: 85 },
+    { id: 11, name: "Tiya", age: "20", marks: 85 },
+  ];
+  arr = arr.concat(arr1);
+
+console.log(arr);
 }
